06a66ef390157e541265a3138f0962957e3c18b9
noname <your.email@example.com>
2023-08-20 04:44:24 +0900

    Add grpc gem and create file about grpc

--- backend/app/sidekiq/get_binance_btc_price_job.rb
+++ backend/app/sidekiq/get_binance_btc_price_job.rb
@@ -5,7 +5,6 @@ class GetBinanceBtcPriceJob
   include UserHelper
 
   def perform(*_args)
-    # BitcoinPrice.USDJPY = 138
     response1 = JSON.parse(Net::HTTP.get(URI.parse('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')))
     response2 = JSON.parse(Net::HTTP.get(URI.parse('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT')))
     binance = { exchange_type: 'BTC_JPY', place: 'Binance', requested_at: floor_sec, ask: (response2['askPrice'].to_f * BitcoinPrice.USDJPY),
