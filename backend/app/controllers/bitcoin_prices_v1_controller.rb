7a1e41b7adb9b13149f353aa33400a1e0b129848
noname <your.email@example.com>
2023-08-22 21:32:05 +0900

    Modify process number of supervisord.conf

--- backend/app/controllers/bitcoin_prices_v1_controller.rb
+++ backend/app/controllers/bitcoin_prices_v1_controller.rb
@@ -1,8 +1,8 @@
 class BitcoinPricesV1Controller < ApplicationController
   wrap_parameters :bitcoin_price, include: [:place, :from, :to, :interval]
   def get
-    logger.debug api_params
-    logger.debug api_params.to_hash.symbolize_keys
+    # logger.debug api_params
+    # logger.debug api_params.to_hash.symbolize_keys
     # price = BitcoinPrice.get(place: params[:place], from: params[:from], to: params[:to], interval: params[:interval])
     price = BitcoinPrice.get(**api_params.to_hash.symbolize_keys)
     render json: price
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# records1 = (1..10_000).map do
+#   {exchange_type: "BTC_JPY",  place: "Coincheck", requested_at: Time.current, ask: 3812194,bid: 3810946,ltp: 3812498,}
+# end
+# BitcoinPrice.insert_all records1
+
+# records2 = (1..10_000).map do
+#   {exchange_type: "BTC_JPY",  place: "bitFlyer", requested_at: Time.current, ask: 3812194,bid: 3810946,ltp: 3812498,}
+# end
+# BitcoinPrice.insert_all records2
+
+# records3 = (1..10_000).map do
+#   {exchange_type: "BTC_JPY",  place: "Binance", requested_at: Time.current, ask: 3812194,bid: 3810946,ltp: 3812498,}
+# end
+# BitcoinPrice.insert_all records3
